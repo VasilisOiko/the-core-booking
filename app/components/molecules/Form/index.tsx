91384b8ddace820995162cf801237b104da9ec61
Vasilis Economou <vasilis_oiko@outlook.com>
2024-02-08 22:24:42 +0200

    feature(form): asbstract component

--- app/components/molecules/Form/index.tsx
+++ app/components/molecules/Form/index.tsx
@@ -1,40 +1,42 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { HiAtSymbol } from "react-icons/hi";
 import { RiLockPasswordFill } from "react-icons/ri";
-import { Input } from "../../atoms/Input";
+import Input from "../../atoms/Input";
 import Button from "../../atoms/Button";
 
-export type useProps = {
+export type useFormProps = {
   className?: any;
+  children?: ReactNode;
+};
+
+export type useItemProps = {
+  className?: any;
+  As?: ReactNode;
+  children?: ReactNode;
 };
 
 const inputContainerStyle = "my-2";
 const symbolStyle = "h-6 w-6 fill-blue-zodiac";
 
-function Form({ className }: useProps) {
+function Form({ className, children }: useFormProps) {
   return (
     <form className={`flex w-72 flex-col items-center ${className}`}>
-      <Input
-        className={`${inputContainerStyle}`}
-        type="email"
-        placeholder="Email Address"
-        id="email"
-        name="email"
-        required={true}
-        icon={<HiAtSymbol className={symbolStyle} />}
-      />
-      <Input
-        className={`${inputContainerStyle}`}
-        type="password"
-        placeholder="Password"
-        id="password"
-        name="password"
-        required={true}
-        icon={<RiLockPasswordFill className={symbolStyle} />}
-      />
-      <Button variant="primary">Login</Button>
+      {children}
     </form>
   );
 }
+
+function Item({className, children}: useItemProps) {
+
+  const containerStyle = "my-2";
+
+  return (
+    <div className={`${containerStyle} ${className}`}>
+      {children}
+    </div>
+  );
+}
+
+Form.Item = Item;
 
 export default Form;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import Form from "../components/molecules/Form";
+import Input from "../components/atoms/Input";
+import Button from "../components/atoms/Button";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { HiOutlineAtSymbol } from "react-icons/hi";
 
 function page() {
+  const symbolStyle = "h-6 w-6";
+
   return (
     <div className="flex h-screen items-center justify-center bg-blurry-gradient">
-      <Form />
+      <Form>
+        <Form.Item>
+          <Input
+          type="email"
+          placeholder="Email Address"
+          id="email"
+          name="email"
+          required={true}
+          icon={<HiOutlineAtSymbol className={`${symbolStyle} stroke-2 stroke-blue-zodiac`} />}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Input
+          type="password"
+          placeholder="Password"
+          id="password"
+          name="password"
+          required={true}
+          icon={<RiLockPasswordFill className={`${symbolStyle} fill-blue-zodiac`} />}
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button variant="primary">Login</Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
