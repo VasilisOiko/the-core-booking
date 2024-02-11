91153b0a5f9a55e7f8bd5dc9c247fc271693ecfb
Vasilis Economou <vasilis_oiko@outlook.com>
2024-02-11 16:53:09 +0200

    fix(dropdown): dropdown will disappear on clicking outside and wont be interactive

--- app/components/molecules/Dropdown/index.tsx
+++ app/components/molecules/Dropdown/index.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {ReactNode, useEffect, useState } from "react";
+import React, {ReactNode, useEffect, useRef, useState } from "react";
 
 export type dropdownProps = {
   className?: any;
@@ -23,6 +23,9 @@ function Dropdown({ className, children, title, onChange}: dropdownProps) {
   const [listVisible, setListVisible] = useState(false);
   const [selectedItem, setSelectedItem] = useState("")
 
+  /* ref list */
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
   /* styling */
   const containerPositionStyle = "fixed"
 
@@ -34,7 +37,7 @@ function Dropdown({ className, children, title, onChange}: dropdownProps) {
   const listShapeStyle = "mt-3 p-3 rounded-bl-3xl";
   const listArrowStyle = "absolute -top-2 right-1 z-0 transform rotate-45 p-2 bg-blue-200 text-white";
   const appearListStyle = "transition ease-out duration-200 translate-y-0 opacity-100";
-  const disappearListStyle = "transition ease-out duration-200 -translate-y-3.5 opacity-0";
+  const disappearListStyle = "transition ease-out duration-200 -translate-y-3.5 opacity-0 pointer-events-none";
 
   const dropdownStyles = [
     baseStyle,
@@ -49,6 +52,22 @@ function Dropdown({ className, children, title, onChange}: dropdownProps) {
   ].join(" ");
 
   /* functionality */
+
+  useEffect(() => {
+    const handleClickOutside = (event:any) => {
+      if(dropdownRef.current?.contains(event.target)) {
+        return
+      }
+      setListVisible(false)
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+    
+  }, [])
   
   // copy children and select by default the first one
   const Items = children?.map((child: any,  index) => {
@@ -79,22 +98,24 @@ function Dropdown({ className, children, title, onChange}: dropdownProps) {
   useEffect(() => {
     onChange(selectedItem);
   }, [selectedItem])
+
   
 
   return (
-    <div className={`${containerPositionStyle} ${className}`}>
-    <button
-    className={`${dropdownStyles}`}
-    onClick={() => setListVisible(!listVisible)}>
-      {title}
-     
+    <div ref={dropdownRef} className={`${containerPositionStyle} ${className}`}>
+      <button
+      className={`${dropdownStyles}`}
+      onClick={() => setListVisible(!listVisible)}>
+        {title}
+      
 
-    </button>
+      </button>
 
       <div className={`${listStyles}`}>
+
        <div className={`${listArrowStyle}`}/>
-       {/* {children} */}
        {Items}
+
       </div>
     
     </div>
