96c0d717b0b33f38dacb0db88ce8d9723453d5b4
Vasilis Economou <vasilis_oiko@outlook.com>
2024-06-26 20:53:49 +0300

    chore(files): convert EOL to LF

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from "react"
-import { Spinner } from "./components"
 
 
 function Page() {
   return (
         <div className="static object-center">
           <h1>page</h1>
-            <Spinner/>
         </div>
   )
 }
