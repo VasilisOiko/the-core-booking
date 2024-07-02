4abe59e94725a24b4b3b1a13afad07767c3732d9
VasilisOiko <vasilis_oiko@outlook.com>
2024-07-02 10:42:59 +0300

    refactor(pages): move pages inside (pages) folder

---
+++ app/(pages)/loading.tsx
@@ -0,0 +1,15 @@
+import { Flex, Spin } from "../components"
+
+const Loading = () => {
+  return (
+    <Flex
+      className="h-screen"
+      justify="center"
+      align="center"
+    >
+      <Spin fullscreen/> 
+    </Flex>
+  )
+}
+
+export default Loading
---
+++ app/(pages)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function Page() {
+  return (
+        <div className="static object-center">
+          <h1>page</h1>
+        </div>
+  )
+}
+
+export default Page
