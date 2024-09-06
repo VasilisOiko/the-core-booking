470370f2f4a84aaf29bd36dba649db4f9cd7e9e4
VasilisOiko <vasilis_oiko@outlook.com>
2024-09-06 16:24:34 +0300

    fix(bookings): implemented Result view when no bookings are available

--- app/components/organisms/BookingPanel/index.tsx
+++ app/components/organisms/BookingPanel/index.tsx
@@ -3,9 +3,12 @@ import {
     BookingsMobileSelectionView,
     DefaultViewport,
     MobileViewport,
+    Result,
 } from "@/app/components"
 
 import { BookingProps } from "@/app/types/bookings"
+import { useTranslations } from "next-intl"
+import { FcBinoculars } from "react-icons/fc"
 
 import React from "react"
 
@@ -14,6 +17,18 @@ type BookingPanelProps = {
 }
 
 function BookingPanel({ bookings }: BookingPanelProps) {
+
+    const t = useTranslations("bookings.page")
+
+    if(bookings.length === 0){
+        return (
+            <Result
+                status="info"
+                title={t("noBookingsView.result.title")}
+                subTitle={t("noBookingsView.result.subtitle")}
+            />
+        )
+    }
 
     return (
         <>
